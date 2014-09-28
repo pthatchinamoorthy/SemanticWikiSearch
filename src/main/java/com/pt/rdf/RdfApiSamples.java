b1356061bbfb3574631525f5850cd6331c2d7812
Thatchinamoorthy, Prabhakaran <thatchinamoorthyp@dnb.com>
2014-09-28 00:17:56 -0500

    Renamed the classes to RDFAPI as these methods would help later

---
+++ src/main/java/com/pt/rdf/RdfApiSamples.java
@@ -0,0 +1,72 @@
+package com.pt.rdf;
+
+import com.hp.hpl.jena.rdf.model.Literal;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.SimpleSelector;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.vocabulary.VCARD;
+
+public class RdfApiSamples {
+	
+	private Model model; 	public Model getModel() {return model;}
+
+	public RdfApiSamples(Model model) {
+		super();
+		this.model = model;
+	}
+	
+	public RdfApiSamples() {
+		super();
+		this.model = ModelFactory.createDefaultModel();
+	}
+	
+	public Model createPT() {
+		Resource nameResource = model.createResource()
+								.addProperty(VCARD.Given, model.createLiteral("Prabhakaran", "en"))
+								.addProperty(VCARD.Family, model.createLiteral("Thatchinamoorthy", "fr"));
+		model.createResource("http://pt.com/PT").
+								addProperty(VCARD.FN, "Prabhakaran Thatchinamoorthy").
+								addProperty(VCARD.N, nameResource);	
+		return model;
+	}
+	
+	public void listResources (){
+		SimpleSelector selector = new SimpleSelector(null, null, (RDFNode)null){
+			public boolean selects(Statement s){
+				return s.getObject().toString().endsWith("y");
+			}
+		};
+		StmtIterator statements = model.listStatements(selector);
+		while (statements.hasNext()){
+			Statement statement = statements.next();
+			Resource subject = statement.getSubject();
+			Property predicate = statement.getPredicate();
+			RDFNode object = statement.getObject();
+			
+			System.out.println("Subject: " + subject);
+			System.out.println("Predicate: "+ predicate);
+			System.out.println("Object: " + object.toString());
+		}
+		
+	}
+	
+	public void writeRDF(){
+		model.write(System.out, "Turtle");
+	}
+
+	
+	/**
+	 * @param args
+	 */
+	public static void main (String [] args){
+		RdfApiSamples dbpedia = new RdfApiSamples();
+		dbpedia.createPT();
+		dbpedia.listResources();
+		dbpedia.writeRDF();
+	}
+}
---
+++ src/test/java/com/pt/rdf/RdfApiSamplesTest.java
@@ -0,0 +1,50 @@
+package com.pt.rdf;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.vocabulary.VCARD;
+
+
+public class RdfApiSamplesTest {
+	
+	RdfApiSamples dbPedia;
+	public static final String personURI = "http://pt.com/PT";
+	public static final String FULL_NAME = "Prabhakaran Thatchinamoorthy";
+	
+	@Before
+	public void setup(){
+		dbPedia = new RdfApiSamples();
+	}
+	
+	
+	@Test
+	public void test_createPT(){
+		dbPedia.createPT();
+		Resource ptResource = dbPedia.getModel().getResource(personURI);
+		Assert.assertEquals(FULL_NAME, ptResource.getProperty(VCARD.FN).getLiteral().getValue());
+		Assert.assertEquals(personURI, ptResource.getURI());
+	}
+	
+	@Test
+	public void test_listResources(){
+		StmtIterator statements = dbPedia.getModel().listStatements();
+		while (statements.hasNext()){
+			Statement statement = statements.next();
+			Resource subject = statement.getSubject();
+			Property predicate = statement.getPredicate();
+			RDFNode object = statement.getObject();
+			
+			System.out.println("Subject: " + subject);
+			System.out.println("Predicate: "+ predicate);
+			System.out.println("Object: " + object);
+		}
+	}
+	
+}
