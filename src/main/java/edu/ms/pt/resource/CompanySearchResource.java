c05889e77f09189dd1bb407b0a212d4db0a96ff7
Thatchinamoorthy, Prabhakaran <thatchinamoorthyp@dnb.com>
2014-10-21 23:53:04 -0500

    Revert "1. added angularjs code to pull data from WebServices"
    This reverts commit 92d8f745197b480deef6cdcac9fbefaba8589dcb.

--- src/main/java/edu/ms/pt/resource/CompanySearchResource.java
+++ src/main/java/edu/ms/pt/resource/CompanySearchResource.java
@@ -1,5 +1,7 @@
 package edu.ms.pt.resource;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -9,7 +11,7 @@ import javax.ws.rs.core.Response;
 
 import org.apache.log4j.Logger;
 
-import edu.ms.pt.sparql.access.Companies;
+import edu.ms.pt.sparql.access.Company;
 import edu.ms.pt.sparql.access.DBPediaSAO;
 
 @Path("search/{company_keyword}")
@@ -22,8 +24,8 @@ public class CompanySearchResource extends Resource{
 	public Response searchCompany(@PathParam("company_keyword") String companyKeyword) {
 		LOGGER.info("The control is now CompanySearch Servlet");
 		
-		Companies companies = new DBPediaSAO().searchCompanyInfo(companyKeyword, uriInfo);
+		List<Company> companyList = new DBPediaSAO().searchCompanyInfo(companyKeyword, uriInfo);
 		
-		return Response.ok().entity(companies).build();
+		return Response.ok().entity(companyList).build();
 	}
 }
