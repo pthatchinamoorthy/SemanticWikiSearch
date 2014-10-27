5e2e8fc20c0506d78f2945b266ea3a91616f4709
Thatchinamoorthy, Prabhakaran <thatchinamoorthyp@dnb.com>
2014-10-27 11:42:34 -0500

    moved the js,images,css into individual folder.

---
+++ src/main/webapp/js/wiki-search.js
@@ -0,0 +1,36 @@
+function CompanySearchController($scope, $http) {
+						
+	$scope.keywordSearch = function(){
+		$scope.ajax=1;
+		$scope.company=null;
+		//$http.get('json/company.json').
+		$http.get('company/search/' + $scope.keyword).										
+			success(function (data) {							
+				$scope.companies = data.companies;
+			}
+		);
+		$scope.ajax=null;
+	}
+					
+	$scope.multipleOptionSearch = function(){					
+			var multipleOptionURL = 'company/mutilple-option-search?' + "companyName=" + $scope.companyName + "&industryName=" + $scope.industryName + "&stockSymbol=" + $scope.stockSymbol + "&keyPeople=" + $scope.keyPeople + "&greaterThanEmployeeSize=" + $scope.greaterThanEmployeeSize + "&lesserThanEmployeeSize=" + $scope.lesserThanEmployeeSize + "&foundedBy=" + $scope.foundedBy + "&income=" + $scope.income + "&revenue=" + $scope.revenue + "&city=" + $scope.city + "&state=" + $scope.state + "&country=" + $scope.country + "&xxx=" + $scope.xxx;
+			alert(multipleOptionURL);																  
+			$http.get($multipleOptionURL).									
+			success(function (data) {
+				$scope.companies = data.companies;
+				}
+			);
+	}
+				
+	$scope.getCompanyInfo = function(companyUrl){		
+		$scope.companies = null;
+		var companyUrl = companyUrl.substring(38); 
+		//var companyUrl = companyUrl.substring(48); // AMAZON
+		$http.get(companyUrl).									
+			success(function (data) {
+				$scope.company = data;
+			}
+		);
+	}
+	
+}
