3dc4eb572339736f44ac6c64dbfcf6f151bc85db
Thatchinamoorthy, Prabhakaran <thatchinamoorthyp@dnb.com>
2014-11-18 19:08:37 -0600

    Added dummy data. Made the add notes input to "TextArea"

--- src/main/java/edu/ms/pt/sparql/access/DBPediaSAO.java
+++ src/main/java/edu/ms/pt/sparql/access/DBPediaSAO.java
@@ -20,6 +20,7 @@ import edu.ms.pt.model.Company;
 public class DBPediaSAO {
 
 	public static final boolean DEPLOY_ENV_AMAZAON = false;
+	private static final boolean DUMMY_DATA = true;
 	
 	private static final Logger LOGGER = Logger.getLogger(DBPediaSAO.class);
 	
@@ -54,40 +55,42 @@ public class DBPediaSAO {
 		sparqlQuery += " } LIMIT 20";
 			
 		QueryExecution queryExecution = null;
+		ResultSet resultSet = null;
+		List<Company> companyList = new ArrayList<Company>();
 		try {
 			LOGGER.log(Priority.INFO, sparqlQuery);
 			queryExecution = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", sparqlQuery);
+			resultSet = queryExecution.execSelect();
+			while(resultSet.hasNext()) {
+				QuerySolution result = resultSet.next();
+				RDFNode organizationIdentifier = result.get("organization");
+				RDFNode name = result.get("nameVar");
+				RDFNode industry = result.get("industryVar");
+				RDFNode country = result.get("locationCountryVar");
+				RDFNode keyPeople = result.get("keyPeopleVar");
+				
+				Company company = new Company();
+				company.setName(name.toString());
+				company.setIndustry(industry != null ? industry.toString() : "-");
+				company.setLocationCountry(country != null ? country.toString() : "-");
+				company.setKeyPeople(keyPeople != null ? keyPeople.toString() : "-");
+				String dataSourceUrl = organizationIdentifier !=  null ? organizationIdentifier.toString() : null;
+				company.setDataSourceUrl(dataSourceUrl);
+				company.setResourceIdentifier(dataSourceUrl.substring(28));
+				
+				companyList.add(company);
+			}	
+			return new Companies(companyList, queryExecution.getQuery().toString());
 		} catch(Exception e) {
 			LOGGER.error(e);
+			if(DUMMY_DATA) {
+				insertDummyCompanyRecords(companyList);
+				return new Companies(companyList, "DBPEdia is down. " + sparqlQuery);
+			}
 			throw e;
 		}
-
-		ResultSet resultSet = queryExecution.execSelect();
-		List<Company> companyList = new ArrayList<Company>();
-		
-		while(resultSet.hasNext()) {
-			QuerySolution result = resultSet.next();
-			RDFNode organizationIdentifier = result.get("organization");
-			RDFNode name = result.get("nameVar");
-			RDFNode industry = result.get("industryVar");
-			RDFNode country = result.get("locationCountryVar");
-			RDFNode keyPeople = result.get("keyPeopleVar");
-			
-			Company company = new Company();
-			company.setName(name.toString());
-			company.setIndustry(industry != null ? industry.toString() : "-");
-			company.setLocationCountry(country != null ? country.toString() : "-");
-			company.setKeyPeople(keyPeople != null ? keyPeople.toString() : "-");
-			String dataSourceUrl = organizationIdentifier !=  null ? organizationIdentifier.toString() : null;
-			company.setDataSourceUrl(dataSourceUrl);
-			company.setResourceIdentifier(dataSourceUrl.substring(28));
-			
-			companyList.add(company);
-		}	
-		return new Companies(companyList, queryExecution.getQuery().toString());
 	}
-	
-	
+
 	/**
 	 * @param searchKeyword
 	 * @param uriInfo 
@@ -136,32 +139,41 @@ public class DBPediaSAO {
 		
 		sparqlQuery += " } LIMIT 20";
 		
-		LOGGER.log(Priority.INFO, "SPARQL Query -->" + sparqlQuery);
-		QueryExecution queryExecution = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", sparqlQuery);
-		
-		ResultSet resultSet = queryExecution.execSelect();
+		QueryExecution queryExecution = null;
+		ResultSet resultSet = null;
 		List<Company> companyList = new ArrayList<Company>();
-		
-		while(resultSet.hasNext()) {
-			QuerySolution result = resultSet.next();
-			RDFNode organizationIdentifierNode = result.get("organization");
-			RDFNode nameNode = result.get("nameVar");
-			RDFNode countryNode = result.get("locationCountryVar");
-			RDFNode keyPeopleNode = result.get("keyPeopleVar");
-			RDFNode industryNode = result.get("industryVar");
-			
-			Company company = new Company();
-			company.setName(nameNode.toString());
-			company.setLocationCountry(countryNode != null ? countryNode.toString() : "-");
-			company.setKeyPeople(keyPeopleNode != null ? keyPeopleNode.toString() : "-");
-			company.setIndustry(industryNode != null ? industryNode.toString() : "-");
-			String dataSourceUrl = organizationIdentifierNode !=  null ? organizationIdentifierNode.toString() : null;
-			company.setDataSourceUrl(dataSourceUrl);
-			company.setResourceIdentifier(dataSourceUrl.substring(28));
+		try {
+			LOGGER.log(Priority.INFO, sparqlQuery);
+			queryExecution = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", sparqlQuery);
+			resultSet = queryExecution.execSelect();
+			while(resultSet.hasNext()) {
+				QuerySolution result = resultSet.next();
+				RDFNode organizationIdentifierNode = result.get("organization");
+				RDFNode nameNode = result.get("nameVar");
+				RDFNode countryNode = result.get("locationCountryVar");
+				RDFNode keyPeopleNode = result.get("keyPeopleVar");
+				RDFNode industryNode = result.get("industryVar");
+				
+				Company company = new Company();
+				company.setName(nameNode.toString());
+				company.setLocationCountry(countryNode != null ? countryNode.toString() : "-");
+				company.setKeyPeople(keyPeopleNode != null ? keyPeopleNode.toString() : "-");
+				company.setIndustry(industryNode != null ? industryNode.toString() : "-");
+				String dataSourceUrl = organizationIdentifierNode !=  null ? organizationIdentifierNode.toString() : null;
+				company.setDataSourceUrl(dataSourceUrl);
+				company.setResourceIdentifier(dataSourceUrl.substring(28));
 
-			companyList.add(company);
-		}	
-		return new Companies(companyList, queryExecution.getQuery().toString());
+				companyList.add(company);
+			}	
+			return new Companies(companyList, queryExecution.getQuery().toString());
+		} catch(Exception e) {
+			LOGGER.error(e);
+			if(DUMMY_DATA) {
+				insertDummyCompanyRecords(companyList);
+				return new Companies(companyList, "DPPedia is down. " + sparqlQuery);
+			}
+			throw e;
+		}
 	}
 
 
@@ -193,36 +205,90 @@ public class DBPediaSAO {
 							"}" +
 							" LIMIT 5";
 		
-		LOGGER.log(Priority.INFO, "SPARQL Query -->" + sparqlQuery);
-		QueryExecution queryExecution = QueryExecutionFactory.create(sparqlQuery);
+		QueryExecution queryExecution = null;
+		ResultSet resultSet = null;
+		try {
+			LOGGER.log(Priority.INFO, sparqlQuery);
+			queryExecution = QueryExecutionFactory.create(sparqlQuery);
+			resultSet = queryExecution.execSelect();
+			Company company = new Company();
+			company.setResourceIdentifier(organisationIdentifier);
+			company.setDataSourceUrl("http://dbpedia.org/resource/" + organisationIdentifier);
+			
+			while(resultSet.hasNext()) {
+				QuerySolution result = resultSet.next();
+				company.setName(result.get("nameVar") !=null ? result.get("nameVar").toString() : "-");
+				company.setIsPrimaryTopicOf(result.get("isPrimaryTopicOfVar") != null ? result.get("isPrimaryTopicOfVar").toString() : "-");
+				company.setAabstract(result.get("abstractVar") != null ? result.get("abstractVar").toString() : "-");
+				company.setFoundedBy(result.get("foundedByVar") != null ? result.get("foundedByVar").toString() : "-");
+				company.setFoundingDate(result.get("foundingDateVar") != null ? result.get("foundingDateVar").toString() : "-");
+				company.setLocationCity(result.get("locationCityVar") != null ? result.get("locationCityVar").toString() : "-");
+				company.setLocationCountry(result.get("locationCountryVar") != null ? result.get("locationCountryVar").toString() : "-");
+				company.setKeyPeople(result.get("keyPeopleVar") != null ? result.get("keyPeopleVar").toString() : "-");
+				company.setSymbol(result.get("symbolVar") != null ? result.get("symbolVar").toString() : "-");
+				company.setRevenue(result.get("revenueVar") != null ? result.get("revenueVar").toString() : "-");
+				company.setNetIncome(result.get("netIncomeVar") != null ? result.get("netIncomeVar").toString() : "-") ;
+				company.setNumEmployees(result.get("numEmployeesVar") != null ? result.get("numEmployeesVar").toString() : "-") ;
+				company.setNotes(result.get("notesVar") != null 
+										? (company.getNotes().contains(result.get("notesVar").toString())  
+												? company.getNotes() 
+												: company.getNotes() + "." + result.get("notesVar").toString()) 
+										: "No Saved Notes for this company");
+				company.setSparqlQuery(queryExecution.getQuery().toString());
+			}	
+			return company;
+		} catch(Exception e) {
+			LOGGER.error(e);
+			if(DUMMY_DATA) {
+				return ReturnCompanyWithDummyData(sparqlQuery);
+			}
+			throw e;
+		}
+	}
 
-		ResultSet resultSet = queryExecution.execSelect();
+	private Company ReturnCompanyWithDummyData(String sparqlQuery) {
 		Company company = new Company();
-		company.setResourceIdentifier(organisationIdentifier);
-		company.setDataSourceUrl("http://dbpedia.org/resource/" + organisationIdentifier);
-		
-		while(resultSet.hasNext()) {
-			QuerySolution result = resultSet.next();
-			company.setName(result.get("nameVar") !=null ? result.get("nameVar").toString() : "-");
-			company.setIsPrimaryTopicOf(result.get("isPrimaryTopicOfVar") != null ? result.get("isPrimaryTopicOfVar").toString() : "-");
-			company.setAabstract(result.get("abstractVar") != null ? result.get("abstractVar").toString() : "-");
-			company.setFoundedBy(result.get("foundedByVar") != null ? result.get("foundedByVar").toString() : "-");
-			company.setFoundingDate(result.get("foundingDateVar") != null ? result.get("foundingDateVar").toString() : "-");
-			company.setLocationCity(result.get("locationCityVar") != null ? result.get("locationCityVar").toString() : "-");
-			company.setLocationCountry(result.get("locationCountryVar") != null ? result.get("locationCountryVar").toString() : "-");
-			company.setKeyPeople(result.get("keyPeopleVar") != null ? result.get("keyPeopleVar").toString() : "-");
-			company.setSymbol(result.get("symbolVar") != null ? result.get("symbolVar").toString() : "-");
-			company.setRevenue(result.get("revenueVar") != null ? result.get("revenueVar").toString() : "-");
-			company.setNetIncome(result.get("netIncomeVar") != null ? result.get("netIncomeVar").toString() : "-") ;
-			company.setNumEmployees(result.get("numEmployeesVar") != null ? result.get("numEmployeesVar").toString() : "-") ;
-			company.setNotes(result.get("notesVar") != null 
-									? (company.getNotes().contains(result.get("notesVar").toString())  
-											? company.getNotes() 
-											: company.getNotes() + "." + result.get("notesVar").toString()) 
-									: "No Saved Notes for this company");
-			company.setSparqlQuery(queryExecution.getQuery().toString());
-		}	
+		company.setName("Dell Inc");
+		company.setIsPrimaryTopicOf("");
+		company.setAabstract("Dell Inc. is an American privately owned multinational computer technology company " +
+				"based in Round Rock, Texas, United States, that develops, sells, repairs and supports computers and" +
+				" related products and services. Bearing the name of its founder, Michael Dell, the company is one of " +
+				"the largest technological corporations in the world, employing more than 103,300 people worldwide.Dell" +
+				" sells personal computers, servers, data storage devices, network switches, software, computer " +
+				"peripherals, HDTVs, cameras, printers, MP3 players and also electronics built by other manufacturers.");
+		company.setFoundedBy("http://dbpedia.org/resource/Michael_Dell");
+		company.setFoundingDate("");
+		company.setLocationCity("http://dbpedia.org/resource/Round_Rock,_Texas");
+		company.setLocationCountry("United States");
+		company.setKeyPeople("http://dbpedia.org/resource/Michael_Dell");
+		company.setSymbol("");
+		company.setRevenue("US$ 56.94 billion");
+		company.setNetIncome("US$ 2.37 billion") ;
+		company.setNumEmployees("108800") ;
+		company.setNotes("This is dummy notes, as DBPedia is down now.");
+		company.setSparqlQuery("DBPedia is down" + sparqlQuery);
 		return company;
 	}
 	
+	private void insertDummyCompanyRecords(List<Company> companyList) {
+		Company company = new Company();
+		company.setName("Dell Inc.");
+		company.setIndustry("Sofware Services");
+		company.setLocationCountry("United States");
+		company.setKeyPeople("Michael Dell");
+		company.setResourceIdentifier("Dell_Inc");				
+		companyList.add(company);
+		company.setName("Microsoft Inc.");
+		company.setIndustry("Computer Software");
+		company.setLocationCountry("United States");
+		company.setKeyPeople("Bill Gates");
+		company.setResourceIdentifier("Microsoft");				
+		companyList.add(company);
+		company.setName("Apple Inc");
+		company.setIndustry("Computer Hardware");
+		company.setLocationCountry("United States");
+		company.setKeyPeople("Tim Cook");
+		company.setResourceIdentifier("Apple_Inc");				
+		companyList.add(company);
+	}
 }
